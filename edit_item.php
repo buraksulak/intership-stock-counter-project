2b0005372da0652e1d215a2e35cd8ddf5a2fe6de
buraksulak <124737696+buraksulak@users.noreply.github.com>
2024-07-31 11:22:56 +0300

    Create edit_item.php

---
+++ edit_item.php
@@ -0,0 +1,25 @@
+<?php
+include 'db.php';
+
+$sql = "SELECT * FROM stock_items";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<h2>Stock Items</h2>";
+    echo "<table border='1'><tr><th>ID</th><th>Name</th><th>Amount</th><th>Description</th><th>Edit</th></tr>";
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>
+                <td>{$row['id']}</td>
+                <td>{$row['name']}</td>
+                <td>{$row['amount']}</td>
+                <td>{$row['description']}</td>
+                <td><a href='edit_item.php?id={$row['id']}'>Edit</a></td>
+              </tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
